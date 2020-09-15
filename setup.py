386530b6b120943a4c08841a409444d522dc5d28
Tom Hennessey <tomahennessey@gmail.com>
2020-09-15 10:14:33 -0500

    Added setup script

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name="subreddit-scraper-tomhennessey",
+        version="0.0.1",
+        author="Tom Hennessey"
+        author_email="tomahennessey@gmail.com"
+        description="A small subreddit scraper"
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/tomhennessey/Subreddit-Scraper"
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires'>=3.6',
+)
