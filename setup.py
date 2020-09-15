53508df8c75748f23402c8f1dcaac7c3175df06a
Tom Hennessey <tomahennessey@gmail.com>
2020-09-15 11:14:49 -0500

    updated package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-        name="subreddit_scraper_tomhennessey",
+        name="subredditscraper_tomhennessey",
         version="0.0.1",
         author="Tom Hennessey",
         author_email="tomahennessey@gmail.com",
