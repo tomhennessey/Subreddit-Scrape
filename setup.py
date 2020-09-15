49edd78d96921826b70ff1dcabad780b0457b307
Tom Hennessey <tomahennessey@gmail.com>
2020-09-15 10:17:14 -0500

    Updated .gitignore; reorganized files

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ with open("README.md", "r") as fh:
 setuptools.setup(
         name="subreddit-scraper-tomhennessey",
         version="0.0.1",
-        author="Tom Hennessey"
-        author_email="tomahennessey@gmail.com"
-        description="A small subreddit scraper"
+        author="Tom Hennessey",
+        author_email="tomahennessey@gmail.com",
+        description="A small subreddit scraper",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        url="https://github.com/tomhennessey/Subreddit-Scraper"
+        url="https://github.com/tomhennessey/Subreddit-Scraper",
         packages=setuptools.find_packages(),
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
-        python_requires'>=3.6',
+        python_requires='>=3.6',
 )
