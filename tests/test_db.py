82ccbee3d83f8f2b06f7fd0d9161d7043bc546cb
Tom Hennessey <tomahennessey@gmail.com>
2020-09-15 10:14:05 -0500

    Added unit tests

---
+++ tests/test_db.py
@@ -0,0 +1,20 @@
+import unittest
+
+import SubredditScrape
+import os
+
+class TestDB(unittest.TestCase):
+    def test_create_connection(self):
+        conn = db.create_connection(r"test.db")
+        self.assertTrue(os.path.exists("test.db"))
+
+    #def test_create_table_submissions(self):
+
+    #def test_create_table_comments(self):
+
+    #def test_insert_submission(self):
+
+    #def test_insert_comment(self): 
+
+if __name__ == '__main__':
+    unittest.main()
